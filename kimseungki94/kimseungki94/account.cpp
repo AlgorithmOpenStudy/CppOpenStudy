fb91b6475a3d9cb01ff45e0e9863cfae6d795da9
kimseungki94 <skkim0832@gmail.com>
2018-12-02 20:48:37 +0900

    account

--- kimseungki94/kimseungki94/account.cpp
+++ kimseungki94/kimseungki94/account.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
+
 typedef struct {
-	char accID[20];
+	int accID;
 	int balance;
 	char cusName[20];
 } Account;
@@ -16,10 +17,15 @@ void mainmenu() {
 	cout << "3. 출금" << endl;
 	cout << "4. 계좌정보 전체 출력" << endl;
 	cout << "5. 프로그램 종료" << endl;
+
 }
 void AddAccount() {
 	cout << "계좌ID : ";
 	cin >> accArr[accpeonum].accID;
+	cin.clear();
+	cin.ignore(100, '\n');
+
+
 	cout << endl << "이름 : ";
 	cin >> accArr[accpeonum].cusName;
 	cout << endl << "입금액 : ";
@@ -28,48 +34,89 @@ void AddAccount() {
 	accpeonum += 1;
 }
 void showAccount() {
-	accpeonum = 0;
-	cout << endl <<"계좌ID : ";
-	cout << accArr[accpeonum].accID;
-	cout << endl << "이름 : ";
-	cout << accArr[accpeonum].cusName;
-	cout << endl << "입금액 : ";
-	cout << accArr[accpeonum].balance;
+
+	for (int i = 0; i < 100; i++) {
+		if (accArr[i].accID != NULL) {
+			cout << endl << "계좌ID : ";
+			cout << accArr[i].accID;
+			cout << endl << "이름 : ";
+			cout << accArr[i].cusName;
+			cout << endl << "입금액 : ";
+			cout << accArr[i].balance << endl;
+		}
+
+	}
+
 }
 void PlusMoney() {
-	char choose[20];
+	int choose;
 	int plusMon;
 	cout << endl;
 	cout << endl;
 	cout << "당신의 accID를 입력하세요 : ";
 	cin >> choose;
-	cout << "얼마를 저축할래요?";
-	cin >> plusMon;
-	accArr[0].balance += plusMon;
-	cout << "현재 돈 : " << accArr[0].balance << endl;
+	cin.clear();
+	cin.ignore(100, '\n');
+	for (int i = 0; i < 100; i++) {
+		if (accArr[i].accID == choose) {
+			cout << "얼마를 저축할래요?";
+			cin >> plusMon;
+			accArr[i].balance += plusMon;
+			cout << "현재 돈 : " << accArr[i].balance << endl;
+		}
 	}
-	
+
+}
 
 void MinusMoney() {
-	char choose[20];
+
+	int choose;
 	int minMon;
 	cout << endl;
 	cout << endl;
 	cout << "당신의 accID를 입력하세요 : ";
 	cin >> choose;
-	cout << "얼마를 뽑을래요?";
-	cin >> minMon;
-	accArr[0].balance -= minMon;
-	cout << "현재 돈 : " << accArr[0].balance << endl;
+	cin.clear();
+	cin.ignore(100, '\n');
+	for (int i = 0; i < 100; i++) {
+		if (accArr[i].accID == choose) {
+			cout << "얼마를 뽑을래요?";
+			cin >> minMon;
+			accArr[i].balance -= minMon;
+			cout << "현재 돈 : " << accArr[i].balance << endl;
+		}
+	}
 }
 
-void main() {
+int main(void) {
+	while (1)
+	{
+		int choose;
+		mainmenu();
+		cout << "선택 : ";
+		cin >> choose;
+		switch (choose)
+		{
+		case 1:
+			AddAccount();
+			break;
+		case 2:
+			PlusMoney();
+			break;
+		case 3:
+			MinusMoney();
+			break;
+		case 4:
+			showAccount();
+			break;
+		case 5:
+			return 0;
+		default:
+			break;
+		}
 
-	mainmenu();
-	AddAccount();
-	showAccount();
-	PlusMoney();
-	MinusMoney();
-	showAccount();
+	}
 	system("pause");
+	return 0;
+
 }
